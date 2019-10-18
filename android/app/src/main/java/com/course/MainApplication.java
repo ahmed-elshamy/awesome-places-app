97c7bfcdc0978604139de41402add5b522f8b455
ahmedshamy <piratethe13@gmail.com>
2019-10-18 16:47:39 +0200

    end map camera module

--- android/app/src/main/java/com/course/MainApplication.java
+++ android/app/src/main/java/com/course/MainApplication.java
@@ -12,6 +12,7 @@ import com.reactnativenavigation.NavigationApplication;
 import com.reactnativenavigation.react.NavigationReactNativeHost;
 import com.reactnativenavigation.react.ReactGateway;
 import com.airbnb.android.react.maps.MapsPackage;
+import com.imagepicker.ImagePickerPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -40,7 +41,8 @@ public class MainApplication extends NavigationApplication {
         return Arrays.<ReactPackage>asList(
             // eg. new VectorIconsPackage()
             new VectorIconsPackage(),
-            new MapsPackage()
+            new MapsPackage(),
+            new ImagePickerPackage()
         );
     }
   
